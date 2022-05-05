86da49fb8fd4b28b2ef41a0d373058d8d7944acb
Rafael Albano <rafaelb.albano@gmail.com>
2022-05-05 16:18:32 -0300

    Criação do componente de copyright

---
+++ src/components/Options/styles.ts
@@ -0,0 +1,8 @@
+import { ChatCenteredText } from "phosphor-react-native";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  }
+});
